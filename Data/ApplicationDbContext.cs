6b6ad541aa6b0dc0603eb733a81111679b23ce1b
Xunnii <baatuubeetoo@gmail.com>
2026-06-28 20:02:36 +0700

    push pertama framework dotnet

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Legaly.Web.Models;
+
+namespace Legaly.Web.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
